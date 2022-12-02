63c596646cc7958960d1a4fcc9d6c2fe111ef61d
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-12-01 20:24:58 -0500

    able to be downloaded

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,17 +18,6 @@ const initdb = async () =>
 // TODO: Add logic for a method that gets all the content from the database
 // export const getDb = async () => console.error('getDb not implemented');
 
-// export const getDb = async () => {
-//   console.log('GET all from the database');
-//   const jateDb = await openDB('jate', 1);
-//   const tx = jateDb.transaction('jate', 'readonly');
-//   const store = tx.objectStore('jate');
-//   const request = store.getAll();
-//   const result = await request;
-//   console.log('result.value', result);
-//   return result;
-// };
-
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
