53c88d1f592d0747bcd9db935f3375d22cd687e6
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-12-01 20:42:30 -0500

    get and put to indexDB functional

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,32 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-// export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error('getDb not implemented');
-
 export const putDb = async (content) => {
-  console.log('PUT to the database');
+  console.log('Post to the Database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, content: content });
+  const request = store.put({ jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
-};
+}
 
+// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from the database');
-  const id = 1;
+  console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.get(id);
+  const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
   return result;
-};
+}
 
 initdb();
 
