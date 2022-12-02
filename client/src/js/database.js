295cc9252927f88c5e93ab507c19e7cd1b1526bd
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-12-01 20:52:24 -0500

    fixed put so it doesnt keep making new database entries

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ jate: content });
+  const request = store.put({ id: 1, jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 }
